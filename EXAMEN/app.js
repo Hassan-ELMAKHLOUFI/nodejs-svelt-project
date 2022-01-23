7295e9438fbf3e613b1ea120638305c3857cf088
Hassan-ELMAKHLOUFI <hassanelmakhloufi2000@gmail.com>
2022-01-23 17:46:07 +0100

    versionFinal

--- EXAMEN/app.js
+++ EXAMEN/app.js
@@ -3,16 +3,21 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-const cors = require('cors');
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var movieRouter = require('./routes/movies');
 var app = express();
 
+const cors = require('cors');
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-app.use(cors())
+
+
+app.use(cors());
+
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
